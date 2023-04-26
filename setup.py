87fa42014d25e28a21563138b47a1f815885eec1
claudiu <klaudyu@gmail.com>
2023-04-26 20:55:13 +0200

    bugfix

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ setup(
     version='0.1',
     description='generate a random network name',
     author='klaudyu',
+    py_modules=['random_name'],
+    packages=find_packages(),
+    package_data={
+	'random_name':['nouns.txt','adjectives.txt'],
+    },
     author_email='klaudyu@gmail.com',
     url='https://github.com/klaudyu/random_name',
     install_requires=['numpy'],
 )
-#py_modules=['random_name'],
-#    package_data={
-#	'random_name':['nouns.txt','adjectives.txt'],
-#    },
